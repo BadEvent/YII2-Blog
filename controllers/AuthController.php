8b44d755840aaa1995d1c39476002fe5a93c92d3
Evgenyi <makarovee98@gmail.com>
2020-09-09 02:04:27 +0700

    login

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -8,14 +8,8 @@ use app\models\User;
 use Yii;
 use yii\web\Controller;
 
-
 class AuthController extends Controller
 {
-    /**
-     * Login action.
-     *
-     * @return Response|string
-     */
     public function actionLogin()
     {
         if (!Yii::$app->user->isGuest) {
@@ -26,9 +20,7 @@ class AuthController extends Controller
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
         }
-
-        $model->password = '';
-        return $this->render('login', [
+        return $this->render('/site/login', [
             'model' => $model,
         ]);
     }
@@ -36,12 +28,54 @@ class AuthController extends Controller
     /**
      * Logout action.
      *
-     * @return Response
+     * @return string
      */
     public function actionLogout()
     {
         Yii::$app->user->logout();
 
         return $this->goHome();
+    }
+
+
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+
+        if(Yii::$app->request->isPost)
+        {
+            $model->load(Yii::$app->request->post());
+            if($model->signup())
+            {
+                return $this->redirect(['auth/login']);
+            }
+        }
+
+        return $this->render('signup', ['model'=>$model]);
+    }
+
+    public function actionLoginVk($uid, $first_name, $photo)
+    {
+        $user = new User();
+        if($user->saveFromVk($uid, $first_name, $photo))
+        {
+            return $this->redirect(['site/index']);
+        }
+    }
+
+    public function actionTest()
+    {
+        $user = User::findOne(1);
+
+        Yii::$app->user->logout();
+
+        if(Yii::$app->user->isGuest)
+        {
+            echo 'Пользователь гость';
+        }
+        else
+        {
+            echo 'Пользователь Авторизован';
+        }
     }
 }
