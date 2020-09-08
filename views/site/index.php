049be248a62fdf8036dfc3da1a919a24e74c1285
Evgenyi <makarovee98@gmail.com>
2020-09-08 20:19:27 +0700

    back to future

--- views/site/index.php
+++ views/site/index.php
@@ -100,7 +100,7 @@ use yii\widgets\LinkPager;
                             <?php foreach ($categories as $category): ?>
                             <li>
                                 <a href="#"><?= $category->title; ?></a>
-                                <span class="post-count pull-right"> (<?= $category->id; ?>)</span>
+                                <span class="post-count pull-right"> (<?= $category->getArticlesCount(); ?>)</span>
                             </li>
                             <?php endforeach; ?>
                         </ul>
