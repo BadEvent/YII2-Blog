07a9dd9dde9b6c11fa00e5fe643b103f833b6039
Evgenyi <makarovee98@gmail.com>
2020-09-08 14:37:53 +0700

    Update ImageUpload.php

--- models/ImageUpload.php
+++ models/ImageUpload.php
@@ -3,9 +3,15 @@
 namespace app\models;
 
 use yii\base\Model;
+use yii\web\UploadedFile;
 
 class ImageUpload extends Model{
 
     public $image;
+
+    public function uploadFile(UploadedFile $file)
+    {
+        $file->saveAs(\Yii::getAlias('@web') . 'uploads/' . $file->name);
+    }
 
 }
