24d4d27adfe7e3305bcd0d27185e3dcf9a66eaaa
Evgenyi <makarovee98@gmail.com>
2020-09-09 02:15:37 +0700

    sum

--- views/auth/login.php
+++ views/auth/login.php
@@ -10,6 +10,9 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="leave-comment mr0"><!--leave comment-->
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
@@ -43,5 +46,12 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="col-lg-offset-1" style="color:#999;">
         You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
         To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+    </div>
+</div>
+        </div>
+
+        <div class="col-md-2">
+
+        </div>
     </div>
 </div>
